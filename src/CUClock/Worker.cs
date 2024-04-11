021e01a5d51389c87eccd0102bd0114b5f4dac6e
Manuel Espinosa <manuel.espinosa@live.com>
2024-04-11 15:04:24 -0600

    Add schedules and log message for en punto y cuarto de hora

--- src/CUClock/Worker.cs
+++ src/CUClock/Worker.cs
@@ -1,12 +1,44 @@
+using Cronos;
+using Humanizer;
+
 namespace CUClock;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly Dictionary<CronExpression, Action> _schedules;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+        _schedules = new Dictionary<CronExpression, Action>();
+        _schedules.Add(CronExpression.Hourly, EnPunto);
+        _schedules.Add(
+            CronExpression.Parse("60/4 * * * * *"),
+            CuartoDeHora);
+    }
+
+    private void EnPunto()
+    {
+        _logger.LogInformation("La hora actual es: {hora}",
+            DateTime.Now.TimeOfDay.Humanize());
+
+
+        _logger.LogInformation("Son las {hora} {meridiano} en punto.",
+            DateTime.Now.TimeOfDay.Hours < 13
+                ? DateTime.Now.TimeOfDay.Hours
+                : DateTime.Now.TimeOfDay.Hours - 12,
+            DateTime.Now.TimeOfDay.Hours < 13
+                ? "del día"
+                : "de la noche");
+    }
+
+    private void CuartoDeHora()
+    {
+        _logger.LogInformation("Son las {hora} y cuarto.",
+            DateTime.Now.TimeOfDay.Hours < 13
+                ? DateTime.Now.TimeOfDay.Hours
+                : DateTime.Now.TimeOfDay.Hours - 12);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)

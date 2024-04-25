2347548344db5c3b10fb83b518f1be8ed5bf1d7a
Manuel Espinosa <manuel.espinosa@live.com>
2024-04-25 11:33:09 -0600

    Comments

--- src/CUClock.Windows.Core/ViewModels/BaseViewModel.cs
+++ src/CUClock.Windows.Core/ViewModels/BaseViewModel.cs
@@ -2,6 +2,9 @@
 
 namespace CUClock.Windows.Core.ViewModels;
 
+/// <summary>
+/// An intermediary between the view and the model.
+/// </summary>
 public abstract class BaseViewModel : ObservableRecipient
 {
 }

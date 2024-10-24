ae5a7d198603895523024b37cb5820034b1db3d6
Manuel Espinosa <manuel.espinosa@live.com>
2024-10-24 13:51:38 -0600

    comments

--- src/CUClock.Windows/Contracts/Services/IAppNotificationService.cs
+++ src/CUClock.Windows/Contracts/Services/IAppNotificationService.cs
@@ -6,6 +6,11 @@ public interface IAppNotificationService
 {
     void Initialize();
 
+    /// <summary>
+    /// Shows a Toast notification with <paramref name="payload"/> as content.
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <returns></returns>
     bool Show(string payload);
 
     NameValueCollection ParseArguments(string arguments);

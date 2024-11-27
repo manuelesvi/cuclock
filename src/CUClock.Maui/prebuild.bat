7fc0f349daa22abd16b4bbd530d6be060f55c21c
Manuel Espinosa <manuel.espinosa@live.com>
2024-11-27 11:39:00 -0600

    add prebuild.bat

---
+++ src/CUClock.Maui/prebuild.bat
@@ -0,0 +1,5 @@
+XCOPY /Y /S /Q ..\aphorismus\src\Aphorismus\Resources\Raw\content .\Resources\Raw\content
+
+REM DEL /Q .\Resources\Raw\content\*.jpg
+REM DEL /Q .\Resources\Raw\content\*.sh
+REM XCOPY /Y /S /Q ..\CUClock.Shared\WAVs .\Resources\Raw\WAVs

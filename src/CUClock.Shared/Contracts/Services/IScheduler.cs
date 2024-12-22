faa729ba39bd153ef7d7fa9821b25fc3cc8d70eb
Manuel Espinosa <manuel.espinosa@live.com>
2024-12-22 12:44:06 -0600

    reoder

--- src/CUClock.Shared/Contracts/Services/IScheduler.cs
+++ src/CUClock.Shared/Contracts/Services/IScheduler.cs
@@ -3,11 +3,14 @@ using CUClock.Shared.Services;
 
 namespace CUClock.Shared.Contracts.Services;
 
+using CronDictionary = IDictionary<
+    CronExpression, Announcer.Schedule>;
+
 public interface IScheduler
 {
+    Task RegisterJobs(CronDictionary jobs);
+
     Task Start();
 
     Task Stop();
-
-    Task RegisterJobs(IDictionary<CronExpression, Announcer.Schedule> jobs);
 }

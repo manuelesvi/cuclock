5a4b3df45e06360a72ae12fcc0063b6b48394d35
Manuel Espinosa <manuel.espinosa@live.com>
2025-01-20 13:50:26 -0600

    Todos checkboxes.

---
+++ src/CUClock.Shared/ViewModels/ChapterDetail.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using Aphorismus.Shared.Entities;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CUClock.Shared.ViewModels;
+
+public sealed class TodosSelectedEventArgs(bool isSelected) : EventArgs
+{
+    public bool IsSelected
+    {
+        get; set;
+    } = isSelected;
+}
+
+public partial class ChapterDetail(Capitulo model) : ObservableRecipient
+{
+    [ObservableProperty]
+    private bool _isSelected = true;
+
+    public int NumeroCapitulo => model.NumeroCapitulo;
+
+    public string Nombre => model.Nombre;
+
+    public bool IsChapterNumVisible => NumeroCapitulo != 0;
+
+    public event EventHandler<TodosSelectedEventArgs> TodosSelected;
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(IsSelected) && NumeroCapitulo == 0)
+        {
+            System.Diagnostics.Debug.WriteLine(
+                $"Chapter '{Nombre}' is selected: {IsSelected}");
+            TodosSelected?.Invoke(this, new TodosSelectedEventArgs(IsSelected));
+        }
+        base.OnPropertyChanged(e);
+    }
+}
--- src/CUClock.Shared/ViewModels/Chapters.cs
+++ src/CUClock.Shared/ViewModels/Chapters.cs
@@ -7,19 +7,37 @@ namespace CUClock.Shared.ViewModels;
 public partial class Chapters : BaseViewModel
 {
     [ObservableProperty]
-    private IList<Capitulo> _items;
+    private IList<ChapterDetail> _items;
 
     public Chapters(IPhraseProvider phraseProvider)
     {
-        var chapters = new List<Capitulo>();
+        var chapters = new List<ChapterDetail>();
+        var todos = new ChapterDetail(new Capitulo { Nombre = "Todos" });
+        todos.TodosSelected += Todos_TodosSelected;
+        chapters.Add(todos);
         for (var i = 0; i < phraseProvider.NumberOfChapters; i++)
         {
-            chapters.Add(new Capitulo
+            chapters.Add(new ChapterDetail(new Capitulo
             {
                 NumeroCapitulo = i + 1,
                 Nombre = phraseProvider.GetChapterName(i + 1)
-            });
+            }));
         }
+        chapters.Add(todos);
         Items = chapters;
+    }
+
+    private void Todos_TodosSelected(object sender, TodosSelectedEventArgs e)
+    {
+        foreach (var d in Items)
+        {
+            if (d.NumeroCapitulo == 0)
+            {
+                continue;
+            }
+            d.IsSelected = e.IsSelected;
+        }
+
+        OnPropertyChanged(nameof(Items));
     }
 }

db4dd003f4b70886c8be5a6f0877ca394bbc20e2
Manuel Espinosa <manuel.espinosa@live.com>
2025-01-21 15:06:48 -0600

    remove comments

--- src/CUClock.Maui/prebuild.bat
+++ src/CUClock.Maui/prebuild.bat
@@ -1,5 +1,1 @@
 XCOPY /Y /S /Q ..\aphorismus\src\Aphorismus\Resources\Raw\content .\Resources\Raw\content
-
-REM DEL /Q .\Resources\Raw\content\*.jpg
-REM DEL /Q .\Resources\Raw\content\*.sh
-REM XCOPY /Y /S /Q ..\CUClock.Shared\WAVs .\Resources\Raw\WAVs

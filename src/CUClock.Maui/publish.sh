d463b16ebde3006905d0b720933361a7f910350a
Manuel Espinosa <manuel.espinosa@live.com>
2025-03-05 14:28:40 -0600

    copy published artifacts to destination directory

--- src/CUClock.Maui/publish.sh
+++ src/CUClock.Maui/publish.sh
@@ -6,3 +6,5 @@ dotnet publish \
     -p:Platform=x64 \
     -p:RuntimeIdentifierOverride=win-x64 
 
+cp -rf ./bin/x64/Release/net9.0-windows10.0.19041.0/win10-x64/publish/* \
+	/c/Users/manchax/cuclock/win10-x64

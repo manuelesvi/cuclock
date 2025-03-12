22ce94aad787adc551340713a023d9cc554cf4bd
Manuel Espinosa <manuel.espinosa@live.com>
2025-03-12 11:21:57 -0600

    set target framework to net10.0-windows10.0.26100.0

--- src/CUClock.Maui/publish.sh
+++ src/CUClock.Maui/publish.sh
@@ -3,10 +3,10 @@
 destination=/c/Users/manchax/cuclock/win10-x64
 
 dotnet publish \
-    -f net9.0-windows10.0.19041.0 \
+    -f net10.0-windows10.0.26100.0 \
     -c Release \
     -p:Platform=x64 \
     -p:RuntimeIdentifierOverride=win-x64 
 
-cp -rf ./bin/x64/Release/net9.0-windows10.0.19041.0/win10-x64/publish/* \
+cp -rf ./bin/x64/Release/net10.0-windows10.0.26100.0/win10-x64/publish/* \
 	$destination

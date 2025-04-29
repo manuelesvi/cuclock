b458f9146c06243def32dc03de3552a25c77626c
Manuel Espinosa <manuel.espinosa@live.com>
2025-04-29 13:42:11 -0600

    set initial window size

--- src/CUClock.Maui/App.xaml.cs
+++ src/CUClock.Maui/App.xaml.cs
@@ -34,22 +34,9 @@ public partial class App : Application
         Window window = new(new AppShell())
         {
             Title = "CUClock, cu cu.",
+            Height = 500,
+            Width = 700
         };
-
-        //window.Stopped += Window_Stopped;
-        //window.Resumed += Window_Resumed;
-
         return window;
-    }
-
-    private void Window_Resumed(object? sender, EventArgs e)
-    {
-        StartScheduler();
-    }
-
-    private void Window_Stopped(object? sender, EventArgs e)
-    {
-        Debugger.Break();
-        //StopScheduler();
     }
 }

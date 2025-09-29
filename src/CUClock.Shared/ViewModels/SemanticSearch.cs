40465b8e866bb4f85114b945ee831714162d2514
Manuel Espinosa <manuel.espinosa@live.com>
2025-09-29 17:53:52 -0600

    search ViewModel and UI bindings

---
+++ src/CUClock.Shared/ViewModels/SemanticSearch.cs
@@ -0,0 +1,43 @@
+﻿using Aphorismus.Shared.Entities;
+using Aphorismus.Shared.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.Logging;
+using System.Threading.Channels;
+using SemanticSearchSVC = CUClock.Shared.Services.SemanticSearch;
+
+namespace CUClock.Shared.ViewModels;
+
+public partial class SemanticSearch : BaseViewModel, IRecipient<SearchResultMessage>
+{
+    private readonly ILogger<SemanticSearch> _logger;
+    private readonly SemanticSearchSVC _searchService;
+
+    public SemanticSearch(
+        SemanticSearchSVC searchService,
+        ILogger<SemanticSearch> logger)
+        : base(logger)
+    {
+        _searchService = searchService;
+        _logger = logger;
+        Search = new RelayCommand<string>(args =>
+            _searchService.SearchChannel.WriteAsync(args));
+        Clean = new RelayCommand(() => Phrases = []);
+        WeakReferenceMessenger.Default.Register(this);
+    }
+
+    [ObservableProperty]
+    public partial IList<Frase> Phrases { get; set; }
+
+    public IRelayCommand<string> Search { get; private set; }
+
+    public IRelayCommand Clean { get; private set; }
+
+    public void Receive(SearchResultMessage message)
+    {
+        _logger.LogInformation("Found {count} matches.",
+            message.Value.Count);
+        Phrases = message.Value;
+    }
+}

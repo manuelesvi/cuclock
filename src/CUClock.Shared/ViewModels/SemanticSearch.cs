e6576a8c2dd06ffae351bc6a580c7f7698b54ac0
Manuel Espinosa <manuel.espinosa@live.com>
2025-10-08 12:16:16 -0600

    speak phrase command

--- src/CUClock.Shared/ViewModels/SemanticSearch.cs
+++ src/CUClock.Shared/ViewModels/SemanticSearch.cs
@@ -3,6 +3,8 @@ using Aphorismus.Shared.Messages;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using CUClock.Shared.Contracts.Services;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
 using SemanticSearchSVC = CUClock.Shared.Services.SemanticSearch;
 
@@ -11,19 +13,28 @@ namespace CUClock.Shared.ViewModels;
 public partial class SemanticSearch : BaseViewModel,
     IRecipient<SearchResultMessage>
 {
+    private readonly IAnnouncer _announcer;
     private readonly ILogger<SemanticSearch> _logger;
     private readonly SemanticSearchSVC _searchService;
 
     public SemanticSearch(
+        IAnnouncer announcer,
         SemanticSearchSVC searchService,
         ILogger<SemanticSearch> logger) : base(logger)
     {
+        _announcer = announcer;
         _searchService = searchService;
         _logger = logger;
         Search = new RelayCommand<string>(async args =>
         {
             if (await _searchService.SearchChannel.WaitToWriteAsync())
                 await _searchService.SearchChannel.WriteAsync(args);
+        });
+        SpeakPhrase = new RelayCommand<Frase>((args) =>
+        {
+            _logger.LogInformation($"Speaking phrase: {args.Capitulo.NumeroCapitulo}.{args.ID}");
+            announcer.SpeakPhrase(args);
+            _logger.LogInformation("Phrase speaked.");
         });
         Clean = new RelayCommand(() => Phrases = []);
         WeakReferenceMessenger.Default.Register(this);
@@ -36,10 +47,12 @@ public partial class SemanticSearch : BaseViewModel,
 
     public IRelayCommand Clean { get; private set; }
 
+    public IRelayCommand<Frase> SpeakPhrase { get; private set; }
+
     public void Receive(SearchResultMessage message)
     {
         _logger.LogInformation("{count} matches found.",
-            message.Value.Count);
-        Phrases = message.Value;
+            message.Value.Count());
+        Phrases = [.. message.Value];
     }
 }
